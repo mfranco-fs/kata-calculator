234b831c38e46f604709e7ed8c346e8ffe815e29
mfranco-fs <mfranco@fastspring.com>
2022-02-09 21:39:05 -0500

    Updated the test cases in order to avoid unnecesary control structures.

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -16,98 +16,47 @@ public class CalculatorTest {
 
     @Test
     public void add_none_returnsDefault(){
-        //Act
-        long result = 0;
-        try{
-            result = calc.add("");
-        } catch(Exception e){
-        }
-        //Assert
-        assertEquals(0,result);
+        assertEquals(0, calc.add(""));
     }
 
     @Test
     public void add_singleValue_returnsSameValue(){
-        //Act
-        long result = 0;
-        try{
-            result = calc.add("5");
-        } catch(Exception e){
-        }
-        //Assert
-        assertEquals(5, result);
+        assertEquals(5, calc.add("5"));
     }
 
     @Test
     public void add_twoNumbers_returnsTheSum(){
-        //Act
-        long result = 0;
-        try{
-            result = calc.add("5,2");
-        } catch(Exception e){
-        }
-        //Assert
-        assertEquals(7, result);
+        assertEquals(7, calc.add("5,2"));
     }
 
     @Test
     public void add_multipleNumbers_returnsTheSum(){
-        //Act
-        long result = 0;
-        try{
-            result = calc.add("5,2,50,100");
-        } catch(Exception e){
-        }
-        //Assert
-        assertEquals(157, result);
+        assertEquals(157, calc.add("5,2,50,100"));
     }
 
     @Test
     public void add_multipleNumbersWithNewLine_returnsTheSum(){
-        //Act
-        long result = 0;
-        try{
-            result = calc.add("5,2,50\n100");
-        } catch(Exception e){
-        }
-
-        //Assert
-        assertEquals(157, result);
+        assertEquals(157, calc.add("5,2,50\n100"));
     }
 
     @Test
     public void add_multipleNumbersWithNewDelimeter_returnsTheSum(){
-        //Act
-        long result = 0;
-        try{
-            result = calc.add("//;\n5,2,50\n100;4");
-        } catch(Exception e){
-        }
-        //Assert
-        assertEquals(161, result);
+        assertEquals(161, calc.add("//;\n5,2,50\n100;4"));
     }
 
     @Test
     public void add_negative_throws1() {
-        //Act
-        try{
-            long result = calc.add("//;\n5,2,50\n100;-4");
-        } catch(Exception e){
-            assertEquals("Negatives not allowed: -4", e.getMessage());
-        }
+        thrown.expect(IllegalArgumentException.class);
+        long result = calc.add("//;\n5,2,50\n100;-4");
     }
 
     @Test
     public void add_negative_throws2() {
-        //Act
-        try{
-            long result = calc.add("//;\n5,2,50\n100;-4,-45");
-        } catch(Exception e){
-            //assert
-            assertEquals("Negatives not allowed: -4,-45", e.getMessage());
-        }
+        thrown.expect(IllegalArgumentException.class);
+        long result = calc.add("//;\n5,2,50\n100;-4,-45");
     }
 
+    //Not recommended, it's better to use the existing rule but leaving for ilustration purposes
     @Test (expected = IllegalArgumentException.class)
     public void add_negative_throws3() {
         long result = calc.add("//;\n5,2,50\n100;-4,-45");
@@ -121,39 +70,18 @@ public class CalculatorTest {
 
     @Test
     public void testForFourDigitNumbers() {
-        //Act
-        long result = 0;
-        try{
-            result = calc.add("//;\n5,2,50\n1000");
-        } catch(Exception e){
-        }
-        //Assert
-        assertEquals(57, result);
+        assertEquals(57, calc.add("//;\n5,2,50\n1000"));
     }
 
 
     @Test
     public void testForMultiCharDelimeters() {
-        //Act
-        long result = 0;
-        try{
-            result = calc.add("//[hola]\n1hola2hola3");
-        } catch(Exception e){
-        }
-        //Assert
-        assertEquals(6, result);
+        assertEquals(6, calc.add("//[hola]\n1hola2hola3"));
     }
 
 
     @Test
     public void testForMultipleDelimeters() {
-        //Act
-        long result = 0;
-        try{
-            result = calc.add("//[hi][there]\n1hi2there3");
-        } catch(Exception e){
-        }
-        //Assert
-        assertEquals(6, result);
+        assertEquals(6, calc.add("//[hi][there]\n1hi2there3"));
     }
 }
